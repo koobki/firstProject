1a35977a449689361fbabb225a4ae228bc10af58
koobk <koobk@DESKTOP-DPC1OT9>
2020-09-02 00:53:41 +0900

    프로그램 수정

--- DefaultProject/src/main/java/edu/sungil/foods/web/controller/DefaultController.java
+++ DefaultProject/src/main/java/edu/sungil/foods/web/controller/DefaultController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import edu.sungil.foods.web.domain.dto.UserInfo;
 import edu.sungil.foods.web.service.DefaultService;
 
@@ -19,14 +20,13 @@ public class DefaultController {
 	@Autowired
 	DefaultService defaultService;
 
-	@RequestMapping(value="/welcome",method=RequestMethod.GET)
-	public void welcome() {
+	@RequestMapping(value="/userInfo",method=RequestMethod.GET)
+	public void getUserInfo() {
 	}
 	
 	@RequestMapping(value="/user", method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity<UserInfo> getUserInfo(@RequestParam(value = "userNo", required = true)Long userNo) {
-		//return ResponseEntity.ok(defaultService.getUserInfo(userNo));
 		return new ResponseEntity<UserInfo>(defaultService.getUserInfo(userNo), HttpStatus.OK);
 	}
 }
--- DefaultProject/src/main/webapp/WEB-INF/jsp/index.jsp
+++ DefaultProject/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<h1>반갑습니다.</h1>
 </body>
 </html>
